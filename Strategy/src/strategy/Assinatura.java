39b30c9d6bc65884895cbec4c942c924c736873d
Aluno <you@example.com>
2019-06-01 11:48:07 -0300

    Strategy

--- Strategy/src/strategy/Assinatura.java
+++ Strategy/src/strategy/Assinatura.java
@@ -1,26 +1,14 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package strategy;
-
-import java.lang.annotation.Target;
-import java.util.Scanner;
 
 /**
  *
  * @author Allan Cassiano
  */
-public class Assinatura {
-      
-
-    /**
-     * @param args the command line arguments
-     */
-    
-    
-    public static void main(String[] args) {        
+public class Assinatura {    
+    public static void main(String[] args) {   
+        
+        
+        
         MontaAssinatura ouvinte = new MontaAssinatura("QualidadeVideo","Preco","QuantidadeTransmissao");
     }
 }
